fd672b0a6471fcb81ebe3d1c81c196398b2eb916
OlgaMakeikina <makeikina2202@gmail.com>
2024-04-12 12:59:06 -0300

    more news are added on the main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import One from './1.jpg';
 import Two from './2.jpg';
 import Three from './3.jpg';
 import Footer from './FooterComponent/footer';
+import NewsApp from "./newsApp";
+
 
 const App = () => {
 
@@ -12,8 +14,10 @@ const App = () => {
 
   return (
     <div>
+      
 <Carousel images={images}/>  
-<h2 className="header_contact">Последние новости</h2>  
+<h2 className="header_main_page">Последние новости</h2>  
+<NewsApp />
 <Footer />
 </div>
 
---
+++ src/newsApp.js
@@ -0,0 +1,74 @@
+import React, { useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { newsData } from './NewComponent/newsData';
+import './App.css'
+
+
+const NewsApp = () => {
+    const sliderRef = useRef();
+  
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        }
+      ]
+    };
+  
+    return (
+      <div className="slider-container">
+        <Slider ref={sliderRef} {...settings}>
+          {newsData.map(newsItem => (
+            <div className='' key={newsItem.id}>
+              <p>{newsItem.date}</p>
+              <h3>{newsItem.title}</h3>
+              <button className='news_btn' onClick={() => navigateToNews(newsItem.url)}>Подробнее &gt;&gt;</button>
+            </div>
+          ))}
+        </Slider>
+
+      <Routes> 
+        {newsData.map(newsItem => (
+          <Route key={newsItem.id} path={newsItem.url} element={<NewsItemContent title={newsItem.title} />} />
+        ))}
+      </Routes>
+    </div>
+  );
+};
+
+const NewsItemContent = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+// Функция для перехода на страницу новости
+const navigateToNews = (url) => {
+  window.location.href = url; // Перенаправляем на указанный URL
+};
+
+export default NewsApp;
