f62e9b37029e1a8d0ee7a1d95fc35c172e570df1
OlgaMakeikina <makeikina2202@gmail.com>
2024-07-05 17:01:58 -0300

    исправлены ссылки в footer

--- src/FooterComponent/footerMap.js
+++ src/FooterComponent/footerMap.js
@@ -10,24 +10,24 @@ const FooterMap = () => {
               <h3>КАРТА САЙТА</h3>
         <ul>
         <li><Link to="/">Главная</Link></li>
-        <li><Link to="/contact">Контакты</Link></li>
-        <li><Link to="/news">Новости</Link></li>
+        <li><Link to="/kontakty">Контакты</Link></li>
+        <li><Link to="/novosti">Новости</Link></li>
         </ul>      
         </div>
     <div>
         <h3>О КОМПАНИИ</h3>
         <ul>
-        <li><Link to="/aboutCompany">О компании</Link></li>
-        <li><Link to="/documents">Учредительные документы и свидетельства</Link></li>
-        <li><Link to="/documents">Платежные реквизиты</Link></li>
+        <li><Link to="/o-kompanii">О компании</Link></li>
+        <li><Link to="/dokumenty">Учредительные документы и свидетельства</Link></li>
+        <li><Link to="/dokumenty">Платежные реквизиты</Link></li>
         </ul>
     </div>
 <div>
         <h3>ЖИТЕЛЯМ</h3>
         <ul>
-        <li><Link to="/houses">Дома в управлении</Link></li>
-        <li><Link to="/people">Информация для населения</Link></li>
-        <li><Link to="/tarifs">Тарифы ЖКХ</Link></li>
+        <li><Link to="/doma-v-upravlenii">Дома в управлении</Link></li>
+        <li><Link to="/informatsiya-dlya-naseleniya">Информация для населения</Link></li>
+        <li><Link to="/tarify-na-zhkh">Тарифы ЖКХ</Link></li>
         </ul>
 </div>
 
