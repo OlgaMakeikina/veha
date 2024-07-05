796c8800202514323d254e525b34208f1eae0257
OlgaMakeikina <makeikina2202@gmail.com>
2024-07-05 16:47:07 -0300

    добавлен yandex.counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
-import One from './1.jpg';
-import Two from './2.jpg';
-import Three from './3.jpg';
 import Footer from './FooterComponent/footer';
 import PosBannerWidget from "./Uslugi";
 import Carousel from "./Carousel";
 import MultipleItems from "./NewsCarouselComponent/multiple";
+import { useEffect } from "react";
 
 const App = () => {
 
-  const images = [One, Two, Three];
+
+  useEffect(() => {
+    // Создаем и вставляем скрипт Яндекс.Метрики
+    (function(m, e, t, r, i, k, a) {
+      m[i] = m[i] || function() {
+        (m[i].a = m[i].a || []).push(arguments);
+      };
+      m[i].l = 1 * new Date();
+      k = e.createElement(t);
+      a = e.getElementsByTagName(t)[0];
+      k.async = 1;
+      k.src = r;
+      a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    // Исключение для ESLint, чтобы определить глобальную переменную ym
+    // eslint-disable-next-line no-undef
+    ym(87981216, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  }, []);
+
 
 
   return (
